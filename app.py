18ac6edf645b7dd915a908abf43c01c1dfb34aa0
Vikrant Thoidingjam <vikrant.th2013@gmail.com>
2023-04-30 10:49:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+
+import streamlit as st
+import pandas as pd
+from transformers import pipeline
+
+model_name = "sshleifer/distilbart-cnn-12-6"
+model_revision = "a4f8f3e"
+
+summarizer = pipeline("summarization", model=model_name, revision=model_revision)
+
+
+# Define a function to generate summaries
+def generate_summary(text):
+    # Generate a summary of the text using the summarization model
+    summary = summarizer(text, max_length=100, min_length=30, do_sample=False)[0]["summary_text"]
+    return summary
+
+# Define the Streamlit app
+
+# Set the title of the app
+st.title("Medicine Summary Generator")
+
+# Add a file uploader to allow the user to upload a CSV file
+st.sidebar.title("Upload a CSV file")
+file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+
+# If a file was uploaded, display a preview of the data
+if file is not None:
+    df = pd.read_csv(file)
+    st.write("Data preview:")
+    st.write(df.head())
+
+# Generate a summary for each medicine in the CSV file
+    st.write("Summary:")
+    for index, row in df.iterrows():
+        summary = generate_summary(row["overview"])
+        st.write(f"{row['drug names']}: {summary}")
